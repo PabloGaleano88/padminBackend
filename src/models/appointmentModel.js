85dfa9dde7fdfed0bbe1c297728b455360bdc9fa
Pablo Galeano <pablogaleano88@hotmail.com>
2025-07-11 10:30:33 -0300

    proximos turnos test6

--- src/models/appointmentModel.js
+++ src/models/appointmentModel.js
@@ -1,6 +1,6 @@
 // src/models/appointmentModel.js
 import mongoose from "mongoose";
-import AppointmentSchema from "../schemas/appointmentSchema.js"; // Asegurate de que esta ruta y archivo existan
+import AppointmentSchema from "../models/schemas/appointmentSchema"; // Asegurate de que esta ruta y archivo existan
 
 const AppointmentModel = mongoose.model("Appointment", AppointmentSchema);
 export default AppointmentModel;
